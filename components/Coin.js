7fede5e9b31f76ad7243ec3018f2621314913ede
Chityanj <chityanj@gmail.com>
2022-05-24 13:19:53 +0530

    refactor

--- components/Coin.js
+++ components/Coin.js
@@ -31,151 +31,30 @@ function Coin() {
         
 return (
     
-    <div className="h-56 w-2/3 grid grid-cols-3 gap-4 md:grid-cols-2 lg:grid-cols-4 flex justify-between items-center relative mx-auto">
-        <div>
-            <div className="bg-white rounded-3xl border shadow-xl p-8 w-auto">
+    <div className="h-56 w-2/3 grid grid-cols-3 gap-4 flex justify-between items-center relative mx-auto">
+        {data.map((v, i)=>(
+            <div key={i.toString()} className="bg-white rounded-3xl border shadow-xl p-8 w-auto">
                 <div className="flex justify-between items-center mb-4">
                 <button className="inline-flex items-center justify-center w-14 h-14 rounded-full">
                     <Image
-                        src={ethimg}
+                        src={require(`../public/coins/${v.symbol}.svg`)}
                         className="h-8 w-8"
                         width="49px" 
                         height="49px"
-                        alt='ETH'
+                        alt={`${v.symbol}`.toUpperCase()}
                         viewBox="0 0 26 26"
                     />
                 </button>
                 <div>
-                    <span className="font-bold text-green-500">{data[0].price_change_24h} ({data[0].price_change_percentage_24h}%)</span><br />
-                    <span className="font-medium text-xs text-gray-500 flex justify-end">Rank: {data[0].market_cap_rank} </span>
+                    <span className={`font-bold text-${parseFloat(v.price_change_percentage_24h)>0?"green":"red"}-500`}>{parseFloat(v.price_change_24h).toPrecision(5)} ({parseFloat(v.price_change_percentage_24h).toPrecision(3)}%)</span><br />
+                    <span className="font-medium text-xs text-gray-500 flex justify-end">Rank: {v.market_cap_rank} </span>
                 </div>
                 </div>
                 <div>
-                <h3 className="font-semibold text-sm text-gray-400">ETH</h3>
-                <h1 className="font-semibold text-xl text-gray-700">$ {data[0].current_price}</h1>
+                <h3 className="font-semibold text-sm text-gray-400">{`${v.symbol}`.toUpperCase()}</h3>
+                <h1 className="font-semibold text-xl text-gray-700">$ {v.current_price}</h1>
                 </div>
-            </div>
-        </div>
-        <div>
-        <div className="bg-white rounded-3xl border shadow-xl p-8 w-auto">
-                <div className="flex justify-between items-center mb-4">
-                <button className="inline-flex items-center justify-center w-14 h-14 rounded-full">
-                    <Image
-                        src={bnbimg}
-                        className="h-8 w-8"
-                        width="49px" 
-                        height="49px"
-                        alt='BNB'
-                        viewBox="0 0 26 26"
-                    />
-                </button>
-                <div>
-                    <span className="font-bold text-green-500">{data[1].price_change_24h} ({data[1].price_change_percentage_24h}%)</span><br />
-                    <span className="font-medium text-xs text-gray-500 flex justify-end">Rank: {data[1].market_cap_rank} </span>
-                </div>
-                </div>
-                <div>
-                <h3 className="font-semibold text-sm text-gray-400">BNB</h3>
-                <h1 className="font-semibold text-xl text-gray-700">$ {data[1].current_price}</h1>
-                </div>
-            </div>
-        </div>
-        <div>
-        <div className="bg-white rounded-3xl border shadow-xl p-8 w-auto">
-                <div className="flex justify-between items-center mb-4">
-                <button className="inline-flex items-center justify-center w-14 h-14 rounded-full">
-                    <Image
-                        src={maticimg}
-                        className="h-8 w-8"
-                        width="49px" 
-                        height="49px"
-                        alt='MATIC'
-                        viewBox="0 0 26 26"
-                    />
-                </button>
-                <div>
-                    <span className="font-bold text-green-500">{data[3].price_change_24h} ({data[3].price_change_percentage_24h}%)</span><br />
-                    <span className="font-medium text-xs text-gray-500 flex justify-end">Rank: {data[3].market_cap_rank} </span>
-                </div>
-                </div>
-                <div>
-                <h3 className="font-semibold text-sm text-gray-400">MATIC</h3>
-                <h1 className="font-semibold text-xl text-gray-700">$ {data[3].current_price}</h1>
-                </div>
-            </div>
-        </div>
-        <div>
-        <div className="bg-white rounded-3xl border shadow-xl p-8 w-auto">
-                <div className="flex justify-between items-center mb-4">
-                <button className="inline-flex items-center justify-center w-14 h-14 rounded-full">
-                    <Image
-                        src={oneimg}
-                        className="h-8 w-8"
-                        width="49px" 
-                        height="49px"
-                        alt='ONE'
-                        viewBox="0 0 26 26"
-                    />
-                </button>
-                <div>
-                    <span className="font-bold text-green-500">{data[5].price_change_24h} ({data[5].price_change_percentage_24h}%)</span><br />
-                    <span className="font-medium text-xs text-gray-500 flex justify-end">Rank: {data[5].market_cap_rank} </span>
-                </div>
-                </div>
-                <div>
-                <h3 className="font-semibold text-sm text-gray-400">ONE</h3>
-                <h1 className="font-semibold text-xl text-gray-700">$ {data[5].current_price}</h1>
-                </div>
-            </div>
-        </div>
-        <div>
-        <div className="bg-white rounded-3xl border shadow-xl p-8 w-auto">
-                <div className="flex justify-between items-center mb-4">
-                <button className="inline-flex items-center justify-center w-14 h-14 rounded-full">
-                    <Image
-                        src={ftmimg}
-                        className="h-8 w-8"
-                        width="49px" 
-                        height="49px"
-                        alt='FTM'
-                        viewBox="0 0 26 26"
-                    />
-                </button>
-                <div>
-                    <span className="font-bold text-green-500">{data[4].price_change_24h} ({data[4].price_change_percentage_24h}%)</span><br />
-                    <span className="font-medium text-xs text-gray-500 flex justify-end">Rank: {data[4].market_cap_rank} </span>
-                </div>
-                </div>
-                <div>
-                <h3 className="font-semibold text-sm text-gray-400">FTM</h3>
-                <h1 className="font-semibold text-xl text-gray-700">$ {data[4].current_price}</h1>
-                </div>
-            </div>
-        </div>
-        <div>
-        <div className="bg-white rounded-3xl border shadow-xl p-8 w-auto">
-                <div className="flex justify-between items-center mb-4">
-                <button className="inline-flex items-center justify-center w-14 h-14 rounded-full">
-                    <Image
-                        src={avaximg}
-                        className="h-8 w-8"
-                        width="49px" 
-                        height="49px"
-                        alt='AVAX'
-                        viewBox="0 0 26 26"
-                    />
-                </button>
-                <div>
-                    <span className="font-bold text-green-500">{data[2].price_change_24h} ({data[2].price_change_percentage_24h}%)</span><br />
-                    <span className="font-medium text-xs text-gray-500 flex justify-end">Rank: {data[2].market_cap_rank} </span>
-                </div>
-                </div>
-                <div>
-                <h3 className="font-semibold text-sm text-gray-400">AVAX</h3>
-                <h1 className="font-semibold text-xl text-gray-700">$ {data[2].current_price}</h1>
-                </div>
-            </div>
-        </div>
+            </div>))}
 </div>
   
 
